161b5a03fb00b1f6e23c9de8206158312a9dc848
Guilherme Beidaki Peres <guibperes@proton.me>
2026-01-08 16:51:05 -0300

    Flatpak: moved package list to script

--- scripts/flatpak.sh
+++ scripts/flatpak.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 echo -e "# Flatpak installation"
-cat $AKIRA_PKG_PATH/flatpak.txt | xargs flatpak install --noninteractive -y flathub
+flatpak install --noninteractive -y flathub \
+  org.gtk.Gtk3theme.Adwaita-dark \
+  io.github.flattool.Warehouse \
+  com.valvesoftware.Steam \
+  com.github.Matoking.protontricks \
+  com.discordapp.Discord \
+  org.gimp.GIMP \
+  md.obsidian.Obsidian \
+  io.gitlab.news_flash.NewsFlash \
+  org.localsend.localsend_app
