87237ba5ecffc646ce0c1c307185f3e62c9bf656
Guilherme Beidaki Peres <guibperes@proton.me>
2026-01-10 02:16:47 -0300

    Script: unified config scripts

--- install.sh
+++ install.sh
@@ -9,8 +9,7 @@ source $AKIRA_DIR_PATH/scripts/yay.sh
 source $AKIRA_DIR_PATH/scripts/flatpak.sh
 source $AKIRA_DIR_PATH/scripts/lazyvim.sh
 source $AKIRA_DIR_PATH/scripts/mise.sh
-source $AKIRA_DIR_PATH/scripts/cp-config.sh
-source $AKIRA_DIR_PATH/scripts/apply-config.sh
+source $AKIRA_DIR_PATH/scripts/config.sh
 source $AKIRA_DIR_PATH/scripts/git.sh
 source $AKIRA_DIR_PATH/scripts/systemd.sh
 echo -e "# Finished Post installation script"
--- scripts/config.sh
+++ scripts/config.sh
@@ -11,3 +11,12 @@ cp $AKIRA_DIR_PATH/assets/akira_wallpaper.jpg $HOME/Pictures/wallpapers
 
 echo -e "# Copying root files with sudo"
 sudo cp -r $AKIRA_CONFIG_PATH/sleep.conf.d /etc/systemd
+
+echo -e "# Applying configurations"
+sudo usermod -aG docker $USER
+sudo chsh -s /bin/fish $USER
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+kitten themes --reload-in all Akira
+xdg-user-dirs-update
+xdg-user-dirs-gtk-update
+fc-cache
