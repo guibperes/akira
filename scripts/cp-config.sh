fa8c232e1276a99a8b38c54188fe2c2805e1bcac
Guilherme Beidaki Peres <guibperes@proton.me>
2026-01-09 22:01:28 -0300

    Config: moved all .config dirs and files to home_config

--- scripts/cp-config.sh
+++ scripts/cp-config.sh
@@ -3,26 +3,10 @@
 echo -e "# Copying configuration files"
 mkdir -p $HOME/Pictures/wallpapers $HOME_CONFIG_PATH/systemd
 
-cp -r \
-  $AKIRA_CONFIG_PATH/calcure \
-  $AKIRA_CONFIG_PATH/dunst \
-  $AKIRA_CONFIG_PATH/eilmeldung \
-  $AKIRA_CONFIG_PATH/fastfetch \
-  $AKIRA_CONFIG_PATH/fish \
-  $AKIRA_CONFIG_PATH/hypr \
-  $AKIRA_CONFIG_PATH/kanshi \
-  $AKIRA_CONFIG_PATH/kitty \
-  $AKIRA_CONFIG_PATH/mpv \
-  $AKIRA_CONFIG_PATH/spotify-player \
-  $AKIRA_CONFIG_PATH/waybar \
-  $AKIRA_CONFIG_PATH/wofi \
-  $AKIRA_CONFIG_PATH/xdg-desktop-portal \
-  $AKIRA_CONFIG_PATH/starship.toml \
-  $HOME_CONFIG_PATH
+cp -r $AKIRA_CONFIG_PATH/home_config $HOME_CONFIG_PATH
 
 cp -r $AKIRA_CONFIG_PATH/ssh $HOME/.ssh
 cp -r $AKIRA_CONFIG_PATH/systemd/user $HOME_CONFIG_PATH/systemd
-
 cp $AKIRA_CONFIG_PATH/.XCompose $HOME/.XCompose
 cp $AKIRA_DIR_PATH/assets/akira_wallpaper.jpg $HOME/Pictures/wallpapers
 
