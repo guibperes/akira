3a505123e9df105817595e3b26b65f53d68baed4
Guilherme Beidaki Peres <guibperes@proton.me>
2026-01-09 22:06:32 -0300

    Config: resolved systemd configs in user and root dirs

--- scripts/cp-config.sh
+++ scripts/cp-config.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 echo -e "# Copying configuration files"
-mkdir -p $HOME/Pictures/wallpapers $HOME_CONFIG_PATH/systemd
+mkdir -p $HOME/Pictures/wallpapers
 
 cp -r $AKIRA_CONFIG_PATH/home_config $HOME_CONFIG_PATH
-
 cp -r $AKIRA_CONFIG_PATH/ssh $HOME/.ssh
-cp -r $AKIRA_CONFIG_PATH/systemd/user $HOME_CONFIG_PATH/systemd
+
 cp $AKIRA_CONFIG_PATH/.XCompose $HOME/.XCompose
 cp $AKIRA_DIR_PATH/assets/akira_wallpaper.jpg $HOME/Pictures/wallpapers
 
 echo -e "# Copying root files with sudo"
-sudo cp -r $AKIRA_CONFIG_PATH/systemd/sleep.conf.d /etc/systemd
+sudo cp -r $AKIRA_CONFIG_PATH/sleep.conf.d /etc/systemd
