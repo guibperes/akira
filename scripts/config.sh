a1f0b480be42add5a92213e23aec663a862a3a0d
Guilherme Beidaki Peres <guibperes@proton.me>
2026-01-29 14:24:29 -0300

    Scripts: organized cp script

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 echo -e "# Copying configuration files"
-mkdir -p $HOME/Pictures/wallpapers
-
 cp -r $AKIRA_CONFIG_PATH/home_config/. $HOME_CONFIG_PATH
-cp -r $AKIRA_CONFIG_PATH/.gnupg $HOME
+cp -r \
+  $AKIRA_CONFIG_PATH/.gnupg \
+  $AKIRA_CONFIG_PATH/.XCompose \
+  $AKIRA_CONFIG_PATH/.gitconfig \
+  $HOME
 
-sed -i "1i @import url(\"$AKIRA_CONFIG_PATH/akira_style/style.css\");" $AKIRA_CONFIG_PATH/wofi/style.css
-
-cp $AKIRA_CONFIG_PATH/.XCompose $AKIRA_CONFIG_PATH/.gitconfig $HOME
-cp $AKIRA_DIR_PATH/assets/akira_wallpaper.jpg $HOME/Pictures/wallpapers
+sed -i \
+  "1i @import url(\"$HOME_CONFIG_PATH/akira_style/style.css\");" \
+  $HOME_CONFIG_PATH/wofi/style.css
 
 echo -e "# Copying root files with sudo"
 sudo cp -r $AKIRA_CONFIG_PATH/sleep.conf.d /etc/systemd
@@ -20,6 +21,7 @@ sudo usermod -aG docker $USER
 sudo chsh -s /bin/fish $USER
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 kitten themes --reload-in all Akira
+swww img $AKIRA_DIR_PATH/assets/akira_wallpaper.jpg
 xdg-user-dirs-update
 xdg-user-dirs-gtk-update
 fc-cache
