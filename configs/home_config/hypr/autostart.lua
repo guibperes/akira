7550109fd54ceeda718fdd76c4425580c39fea61
Guilherme Beidaki Peres <guibperes@proton.me>
2026-06-27 13:41:35 -0300

    Hyprland: added autostart apps

--- configs/home_config/hypr/autostart.lua
+++ configs/home_config/hypr/autostart.lua
@@ -1,7 +1,12 @@
+local config = require("variables")
+
 hl.on("hyprland.start", function()
   hl.exec_cmd("systemctl --user start hyprpolkitagent")
   hl.exec_cmd("hypridle")
   hl.exec_cmd("awww-daemon")
   hl.exec_cmd("waybar")
   hl.exec_cmd("kanshi")
+
+  hl.exec_cmd(config.browser, { workspace = "1 silent" })
+  hl.exec_cmd("kitty eilmeldung", { workspace = "6 silent" })
 end)
