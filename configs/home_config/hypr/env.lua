f5bb4b15daa3d3bdbf1dea97d19497487d8a5412
Guilherme Beidaki Peres <guibperes@proton.me>
2026-06-27 13:11:30 -0300

    Hyprland: fixed env value for SSH_AUTH_SOCK

--- configs/home_config/hypr/env.lua
+++ configs/home_config/hypr/env.lua
@@ -1,4 +1,4 @@
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
-hl.env("SSH_AUTH_SOCK", "$XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh")
+hl.env("SSH_AUTH_SOCK", os.getenv("XDG_RUNTIME_DIR") .. "/gnupg/S.gpg-agent.ssh")
 hl.env("GTK_IM_MODULE", "simple")
